a98fefea2ee44065ed6adb456f80e13fbc8f3732
ferdy jiu <>
2021-03-26 16:52:25 +0700

    initial commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'
+// Mount Function to start up the app
+
+const mount = (el) => {
+    ReactDOM.render(
+        <App></App>,
+        el
+    );
+};
+
+
+// IF we are in development and in isolation 
+// call mount immediately
+if(process.env.NODE_ENV == 'development'){
+    const devRoot = document.querySelector('#_marketing-dev-root');
+    if(devRoot){
+        mount(devRoot);
+    }
+}
+
+export {mount}
+
+// We are running thourc container
